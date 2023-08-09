5c5f777f914a44f30f459d89042bc0e0e8f5428e
Chris Wong <cwong@anyscale.com>
2023-08-09 09:41:44 -0700

    save

--- build-anyscale-server.sh
+++ build-anyscale-server.sh
@@ -1,7 +1,7 @@
 # export DOCKER_DEFAULT_PLATFORM=linux/amd64
 # cd .devcontainer
 # docker build . -t vscode-rayml
-# docker run -v /Users/sofian/src:/home/ray --platform=linux/amd64 vscode-rayml
+# docker run -v /Users/cwong/vscode:/home/ray/vscode vscode-rayml
 yarn
 yarn gulp vscode-reh-web-linux-x64-min
 tar -czf vscode-server-anyscale.tar ../vscode-reh-web-linux-x64
